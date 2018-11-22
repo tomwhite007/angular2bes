a30d01fd0bbb7d1ba4ffce03686c74ad744cf4f1
Tom White <tomwhite007@gmail.com>
2018-11-22 21:34:35 +0000

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,73 @@
+var gulp = require('gulp');
+var clean = require('gulp-clean');
+var browserSync = require('browser-sync').create();
+var pkg = require('./package.json');
+
+// Copy vendor files from /node_modules into /vendor
+// NOTE: requires `npm install` before running!
+gulp.task('copy', function() {
+  gulp.src([
+      'node_modules/bootstrap/dist/**/*',
+      '!**/npm.js',
+      '!**/bootstrap-theme.*',
+      '!**/*.map'
+    ])
+    .pipe(gulp.dest('vendor/bootstrap'))
+
+  gulp.src(['node_modules/jquery/dist/jquery.js', 'node_modules/jquery/dist/jquery.min.js'])
+    .pipe(gulp.dest('vendor/jquery'))
+
+  gulp.src(['node_modules/jquery.easing/*.js'])
+    .pipe(gulp.dest('vendor/jquery-easing'))
+})
+
+gulp.task('dist', function() {
+
+  // gulp.src('dist', {read: false})
+  // .pipe(clean({force: true}))
+
+  gulp.src([
+      'vendor/**/*'
+    ])
+    .pipe(gulp.dest('dist/vendor'))
+
+  gulp.src([
+    'css/**/*'
+  ])
+  .pipe(gulp.dest('dist/css'))
+
+  gulp.src([
+    'assets/**/*'
+  ])
+  .pipe(gulp.dest('dist/assets'))
+
+  gulp.src([
+    'js/**/*'
+  ])
+  .pipe(gulp.dest('dist/js'))
+
+  gulp.src([
+    'index.html'
+  ])
+  .pipe(gulp.dest('dist'))
+})
+
+// Default task
+gulp.task('default', ['copy']);
+
+// Configure the browserSync task
+gulp.task('browserSync', function() {
+  browserSync.init({
+    server: {
+      baseDir: ''
+    },
+  })
+})
+
+// Dev task with browserSync
+gulp.task('dev', ['browserSync'], function() {
+  // Reloads the browser whenever HTML or CSS files change
+  gulp.watch('css/*.css', browserSync.reload);
+  gulp.watch('js/*.js', browserSync.reload);
+  gulp.watch('*.html', browserSync.reload);
+});
